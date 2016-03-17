b4197c1f974e2197d4758676e95493a46f0421fe
Lee Azzarello <lee@seriesdigital.com>
2016-03-16 21:09:55 -0700

    let's use node-router afterall, because fun times!

--- api-server.js
+++ api-server.js
@@ -1,8 +1,6 @@
-var NodeRouter = require("node-router");
 var SerialPort = require("serialport").SerialPort;
 var sprintf = require("sprintf").sprintf;
-var server = require("http");
-
+var server = require("node-router").getServer();
 var protocol = require("prolite-protocol.json");
 
 var unit_id = "01";
@@ -10,10 +8,10 @@ var program_id = "PC";
 var scroll_type = "FD";
 var input_length = 1023;
 var addr = "127.0.0.1";
-var port = "8888";
+var port = 8888;
 var serial_device = "/dev/ttyS0";
 
-var message_string = sprintf("<ID%s><%s><%s> Hello World! ", unit_id, program_id, scroll_type);
+//var message_string = sprintf("<ID%s><%s><%s> Hello World! \r\n", unit_id, program_id, scroll_type);
 
 serialPort = new SerialPort(serial_device, {
   baudRate: 300,
@@ -24,9 +22,24 @@ function parse_input(protocol, input) {
   return input;
 }
 
-server.listen(addr + port, function( req, resp) {
-
-  if ( req.payload.length > input_length ) {
+server.get("/marquee", function( req, resp, match) {
+  if ( req.body.length > input_length ) {
     return "GTFO";
+  } else {
+    var message_string = sprintf("<ID%s><%s><%s> Hello World! \r\n", unit_id, program_id, scroll_type);
   }
+
+  serialPort.open(function(err) {
+    if ( err ) {
+      console.log('GTFO! No serial port. Exiting... ' + err);
+    } else {
+      serialPort.write(message_string, function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+      });
+    }
+  });
+  return {response: "the sign says " + req.body};
 });
+
+server.listen(addr + ":" + port);
