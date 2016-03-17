90ea4b778780b8aac1a6e271eefc5bcec3f8b9af
Lee Azzarello <lee@seriesdigital.com>
2016-03-16 20:40:38 -0700

    SERIAL PORT!!!

--- api-server.js
+++ api-server.js
@@ -11,9 +11,15 @@ var scroll_type = "FD";
 var input_length = 1023;
 var addr = "127.0.0.1";
 var port = "8888";
-var serial_port = "/dev/ttyS0";
+var serial_device = "/dev/ttyS0";
 
 var message_string = sprintf("<ID%s><%s><%s> Hello World! ", unit_id, program_id, scroll_type);
+
+serialPort = new SerialPort(serial_device, {
+  baudRate: 300,
+  flowControl: false
+});
+
 function parse_input(protocol, input) {
   return input;
 }
