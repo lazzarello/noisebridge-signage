3c59f472589705bdb23d44beeb6337d0531641c8
Lee Azzarello <lee@seriesdigital.com>
2016-03-16 20:38:11 -0700

    listen stuff

--- api-server.js
+++ api-server.js
@@ -1,6 +1,7 @@
 var NodeRouter = require("node-router");
 var SerialPort = require("serialport").SerialPort;
 var sprintf = require("sprintf").sprintf;
+var server = require("http");
 
 var protocol = require("prolite-protocol.json");
 
@@ -8,13 +9,20 @@ var unit_id = "01";
 var program_id = "PC";
 var scroll_type = "FD";
 var input_length = 1023;
+var addr = "127.0.0.1";
+var port = "8888";
+var serial_port = "/dev/ttyS0";
 
 var message_string = sprintf("<ID%s><%s><%s> Hello World! ", unit_id, program_id, scroll_type);
-
-if ( body.payload.length > input_length ) {
-  return "GTFO";
-}
-
 function parse_input(protocol, input) {
   return input;
+}
+
+server.listen(addr + port, someshit) {
+
+  if ( body.payload.length > input_length ) {
+    return "GTFO";
+  }
+
+  return "WTF";
 }
