77ebd02dc59b3ce1660b0e8904a68da47f7889ce
Lee Azzarello <lee@seriesdigital.com>
2016-03-16 20:31:47 -0700

    stub stub stub input function, validation

--- api-server.js
+++ api-server.js
@@ -7,5 +7,14 @@ var protocol = require("prolite-protocol.json");
 var unit_id = "01";
 var program_id = "PC";
 var scroll_type = "FD";
+var input_length = 1023;
 
 var message_string = sprintf("<ID%s><%s><%s> Hello World! ", unit_id, program_id, scroll_type);
+
+if ( body.payload.length > input_length ) {
+  return "GTFO";
+}
+
+function parse_input(protocol, input) {
+  return input;
+}
